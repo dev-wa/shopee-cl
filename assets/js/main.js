c15bb195e9d15ad698604726ccf588d53c69a9c6
Dev-web <sgtonyn@gmail.com>
2023-09-15 15:38:40 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,434 @@
+const data = [
+  {
+    product_name: "Nước Tẩy Trang L’Oreal Micellar Water 3-in-1 (400ml)",
+    product_image: "sg-11134201-22110-we1h1htko0jvbc_tn",
+    product_price_old: "",
+    product_price: "₫49.000 - ₫209.000",
+    product_percent: "46%",
+    product_sold: "Đã bán 25k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name:
+      "Tẩy Tế Bào Chết Toàn Thân Exclusive Cosmetic Chiết Xuất Quế Hồi & Cafe (380g)",
+    product_image: "vn-11134207-7qukw-liihn7btet3mac_tn",
+    product_price_old: "₫140.000",
+    product_price: "₫80.000",
+    product_percent: "43%",
+    product_sold: "Đã bán 104,3k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Kem Ủ Trắng Secret Key Snow White Milky Pack (200ml)",
+    product_image: "sg-11134201-23010-014aam703ymv0b_tn",
+    product_price_old: "₫320.000",
+    product_price: "₫239.000",
+    product_percent: "25%",
+    product_sold: "Đã bán 117,7k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Bột Đậu Đỏ Nguyên Chất 100% Mizuki",
+    product_image: "vn-11134207-7r98o-lkp45apkltowb4_tn",
+    product_price_old: "₫35.000",
+    product_price: "₫19.000",
+    product_percent: "46%",
+    product_sold: "Đã bán 808",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Muối Tắm Tẩy Tế Bào Chết A Bonné (350g)",
+    product_image: "vn-11134201-23020-xirqr55wmsnve9_tn",
+    product_price_old: "₫35.000",
+    product_price: "₫19.900",
+    product_percent: "43%",
+    product_sold: "Đã bán 23,3k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Nước Tẩy Trang Cho Mọi Loại Da Garnier Micellar Water",
+    product_image: "vn-11134207-7qukw-lkhyjp0dakko94_tn",
+    product_price_old: "",
+    product_price: "₫39.000 - ₫169.000",
+    product_percent: "29%",
+    product_sold: "Đã bán 1,2k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua để nhận quà"
+  },
+  {
+    product_name: "Nước Tẩy Trang Byphasse Solution Micellaire (500ml)",
+    product_image: "vn-11134207-7r98o-lkvs8basfw0bbe_tn",
+    product_price_old: "",
+    product_price: "₫35.000 - ₫98.000",
+    product_percent: "47%",
+    product_sold: "Đã bán 23,2k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Nước Hoa Hồng Diếp Cá Tía Tô Chinoshio Dokudami Natural Skin Lotion (500ml)",
+    product_image: "b499704e803078a812198fd393a7ba4f_tn",
+    product_price_old: "₫129.000",
+    product_price: "₫79.000",
+    product_percent: "39%",
+    product_sold: "Đã bán 2,9k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Sữa Chống Nắng Dưỡng Da Trắng Mịn Sunplay Skin Aqua Clear White Limited Editon SPF 50+, PA++++",
+    product_image: "sg-11134201-23010-qd5f0lnak1lvff_tn",
+    product_price_old: "",
+    product_price: "₫77.000 - ₫259.000",
+    product_percent: "26%",
+    product_sold: "Đã bán 4,1k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Bông Tẩy Trang Ipek (150 miếng)",
+    product_image: "d1a8d594ee2a42d49a46bf704323f1ae_tn",
+    product_price_old: "₫35.000",
+    product_price: "₫28.000",
+    product_percent: "20%",
+    product_sold: "Đã bán 85,2k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name:
+      "Sữa Tắm Dưỡng Ẩm Và Làm Sáng Da Hatomugi Reihaku Hatomugi Body Soap (800ml)",
+    product_image: "ce753065ad4e4a0d2554c61b194757de_tn",
+    product_price_old: "₫159.000",
+    product_price: "₫84.000",
+    product_percent: "47%",
+    product_sold: "Đã bán 39,4k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Tẩy Tế Bào Chết Huxley Secret Of Sahara Scrub Mask Sweet Therapy (30g)",
+    product_image: "7e108d3487ea59d465eff150d6221e4b_tn",
+    product_price_old: "₫100.000",
+    product_price: "₫62.000",
+    product_percent: "38%",
+    product_sold: "Đã bán 44,8k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Gel Tẩy Da Chết Rosette Gommage Peeling Gel (120g)",
+    product_image: "79614fad3e6bbc2eea56deb7fb9c2d99_tn",
+    product_price_old: "",
+    product_price: "₫83.000 - ₫129.000",
+    product_percent: "45%",
+    product_sold: "Đã bán 12,9k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Sữa Rửa Mặt Dịu Nhẹ Cetaphil Gentle Skin Cleanser (500ml)",
+    product_image: "1d1487f49148de1fd2501fb4aeb21407_tn",
+    product_price_old: "",
+    product_price: "₫132.000 - ₫345.000",
+    product_percent: "31%",
+    product_sold: "Đã bán 6,7k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Sữa Rửa Mặt Hatomugi Cleansing & Facial Washing The Facial Foam (130g)",
+    product_image: "48c9fca4120646e15f297ef4cbf37365_tn",
+    product_price_old: "₫99.000",
+    product_price: "₫49.500",
+    product_percent: "50%",
+    product_sold: "Đã bán 637",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Sữa Rửa Mặt Shiseido Senka Perfect Whip Cleansing Foam (120g)",
+    product_image: "1364d9e3302c705878c665c3d2ed1596_tn",
+    product_price_old: "",
+    product_price: "₫65.000 - ₫83.000",
+    product_percent: "50%",
+    product_sold: "Đã bán 3,5k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Kem Đánh Trắng Răng Eucryl Toothpaste (62g)",
+    product_image: "274021043f8bff40903bbc6021e8f7f5_tn",
+    product_price_old: "₫60.000",
+    product_price: "₫39.000",
+    product_percent: "35%",
+    product_sold: "Đã bán 8,7k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Nước tẩy trang Eveline Hyaluron Clinic B5 Puridetox (500ml)",
+    product_image: "vn-11134207-7qukw-lkixyilthtp8e3_tn",
+    product_price_old: "₫250.000",
+    product_price: "₫175.000",
+    product_percent: "30%",
+    product_sold: "Đã bán 9k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Gel Rửa Mặt Cosrx Good Morning Low PH Cleanser (150ml)",
+    product_image: "vn-11134207-7qukw-ljs2mp71jfgi10_tn",
+    product_price_old: "₫239.000",
+    product_price: "₫127.000",
+    product_percent: "47%",
+    product_sold: "Đã bán 5,2k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Xà Phòng Lưng Pelican For Back Medicated Soap (135g)",
+    product_image: "2457128b2c21d70ffbb1b2e1e04cf63f_tn",
+    product_price_old: "₫135.000",
+    product_price: "₫74.000",
+    product_percent: "45%",
+    product_sold: "Đã bán 21,5k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Bột Tẩy Trắng Răng Eucryl Toothpowder (50g)",
+    product_image: "b124665b4e1eb713267374f1d12c9c48_tn",
+    product_price_old: "₫65.000",
+    product_price: "₫39.000",
+    product_percent: "40%",
+    product_sold: "Đã bán 12,7k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Gel Giảm Thâm, Dưỡng Da Gamma Megaduo Gel (15g)",
+    product_image: "5301cacfae148a646deb39f5d28138d4_tn",
+    product_price_old: "",
+    product_price: "₫95.000 - ₫139.000",
+    product_percent: "22%",
+    product_sold: "Đã bán 1,8k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Nước Tẩy Trang Senka All Clear Water Micellar Formula (230ml)",
+    product_image: "c038663816ece0b81ed3684906109a3f_tn",
+    product_price_old: "₫110.000",
+    product_price: "₫85.000",
+    product_percent: "23%",
+    product_sold: "Đã bán 2,1k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name: "Tinh Chất Timeless Vitamin B5 + Hyaluronic Acid (30ml)",
+    product_image: "vn-11134207-7r98o-llio3z4goiiede_tn",
+    product_price_old: "₫670.000",
+    product_price: "₫389.000",
+    product_percent: "42%",
+    product_sold: "Đã bán 8,2k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: ""
+  },
+  {
+    product_name:
+      "Sữa Rửa Mặt Thải Độc Rau Củ Wonder Bath Super Vegitoks Cleanser (300ml)",
+    product_image: "vn-11134207-7qukw-lkapazcr9cbg86_tn",
+    product_price_old: "",
+    product_price: "₫145.000 - ₫219.000",
+    product_percent: "49%",
+    product_sold: "Đã bán 1,8k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Kem Chống Nắng Dạng Tinh Chất L’oreal New UV Defender UV Serum Protector SPF50+/PA++++ (50ml)",
+    product_image: "vn-11134207-7r98o-lkrnh49qwuyo6b_tn",
+    product_price_old: "₫349.000",
+    product_price: "₫279.000",
+    product_percent: "20%",
+    product_sold: "Đã bán 1,8k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Gel Dưỡng Ẩm Làm Hồng Môi Mediheal Labocare Pantenolips (10ml)",
+    product_image: "60c79db1975f2bd6a9689e45655225c8_tn",
+    product_price_old: "₫109.000",
+    product_price: "₫62.000",
+    product_percent: "43%",
+    product_sold: "Đã bán 3,9k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Son Dưỡng Mềm Môi Vaseline Stick (4.8g)",
+    product_image: "sg-11134201-23020-gvh5n9rgk8mv31_tn",
+    product_price_old: "",
+    product_price: "₫45.000 - ₫59.000",
+    product_percent: "22%",
+    product_sold: "Đã bán 2,1k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name: "Sữa Rửa Mặt CeraVe Foaming Facial Cleanser",
+    product_image: "sg-11134201-23020-652m1zjuoenv34_tn",
+    product_price_old: "",
+    product_price: "₫309.000 - ₫385.000",
+    product_percent: "13%",
+    product_sold: "Đã bán 2,1k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  },
+  {
+    product_name:
+      "Combo Simple (Sữa Rửa Mặt + Nước Tẩy Trang + Nước Hoa Hồng + Kem Dưỡng Ẩm Da)",
+    product_image: "vn-11134207-7qukw-lkgjevicwzvgd0_tn",
+    product_price_old: "",
+    product_price: "₫105.000 - ₫149.000",
+    product_percent: "32%",
+    product_sold: "Đã bán 10k",
+    product_origin: "Hà Nội",
+    product_shopGood: "",
+    product_Deal_Combo: "Mua Kèm Deal Sốc"
+  }
+];
+
+const productView = document.getElementById("product-view");
+
+const htmls = data.map((item) => {
+  const priceWithHTML = item.product_price.replace(
+    /₫/g,
+    '<span class="currency">&#8363;</span>'
+  );
+  const priceOldWithHTML = item.product_price_old.replace(
+    /₫/g,
+    '<span class="currency_old">&#8363;</span>'
+  );
+  const html = `
+    <div class="col l-2-4 m-4 c-6">
+        <!-- Product item -->
+        <a href="" class="home-product-item">
+            <div class="home-product-item__img"
+                style="background-image: url('./assets/img/items/${
+                  item.product_image
+                }');">
+
+            </div>
+            <h4 class="home-product-item__name">${item.product_name}
+            </h4>
+            <div class="home-product-item__deal">
+                <div class="home-product-item__percent" style="color: rgb(246, 145, 19);">
+
+                    <svg class="home-product-item__percent-svg" viewBox="-0.5 -0.5 4 16">
+                        <path d="M4 0h-3q-1 0 -1 1a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3q0 1 1 1h3" stroke-width="1" transform="" stroke="currentColor" fill="#f69113">
+                        </path>
+                    </svg>
+
+                    <div class="home-product-item__percent-label" style="color: white; background-color: rgb(246, 145, 19);">10% Giảm</div>
+
+                    <svg class="home-product-item__percent-svg" viewBox="-0.5 -0.5 4 16">
+                        <path d="M4 0h-3q-1 0 -1 1a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3q0 1 1 1h3" stroke-width="1" transform="rotate(180) translate(-3 -15)" stroke="currentColor" fill="#f69113">
+                        </path>
+                    </svg>
+                </div>
+                <span class="${
+                  item.product_Deal_Combo ? "home-product-item__deal-combo" : ""
+                }">${item.product_Deal_Combo}</span>
+                <span class="home-product-item__price-current"></span>
+            </div>
+            <div class="home-product-item__price">
+                <span class="home-product-item__price-old" style="${
+                  item.product_price_old ? "margin-right: 4px;" : ""
+                }">${priceOldWithHTML}</span>
+                <span class="home-product-item__price-current">${priceWithHTML}</span>
+            </div>
+            <div class="home-product-item__action">
+                <!-- <span class="home-product-item__like home-product-item__like--liked">
+                    <i class="home-product-item__like-icon-empty far fa-heart"></i>
+                    <i class="home-product-item__like-icon-fill fas fa-heart"></i>
+                </span>-->
+                <div class="home-product-item__rating">
+                    <i class="home-product-item__star--gold fas fa-star"></i>
+                    <i class="home-product-item__star--gold fas fa-star"></i>
+                    <i class="home-product-item__star--gold fas fa-star"></i>
+                    <i class="home-product-item__star--gold fas fa-star"></i>
+                    <i class="home-product-item__star--gold fas fa-star"></i>
+                </div>
+                <span class="home-product-item__sold">${
+                  item.product_sold
+                }</span>
+            </div>
+            <div class="home-product-item__origin">
+                <!-- <span class="home-product-item__brand">Ohui</span>-->
+                <span class="home-product-item__origin-name">${
+                  item.product_origin
+                }</span>
+            </div>
+            <div class="home-product-item__favourite">
+                <i class="fas fa-check"></i>
+                <span>Yêu thích</span>
+            </div>
+            <div class="home-product-item__sale-off">
+                <span class="home-product-item__sale-off-percent">${
+                  item.product_percent
+                }</span>
+                <span class="home-product-item__sale-off-label">GIẢM</span>
+            </div>
+        </a>
+    </div>
+    
+    `;
+
+  return html;
+});
+productView.innerHTML = htmls.join("");
